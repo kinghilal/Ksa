353caef7494af031d15ce20f2d9f1fcb4603eadb
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-01-25 13:54:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("prefix : ++",{type: 'WATCHING'})
+   client.user.setActivity("prefix : +",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
